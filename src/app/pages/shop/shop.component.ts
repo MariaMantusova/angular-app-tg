28f0e39c3093c46258ae149594f3934a8ee51baa
MariaMantusova <maria.mantusova@mail.ru>
2024-05-31 12:56:46 +0300

    feat: add products list

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
+import { ProductsService } from '../../services/products.service';
+import { TelegramService } from '../../services/telegram.service';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
-  templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
-})
-export class ShopComponent {
+  imports: [ProductListComponent],
+  template: `
+   <app-product-list
+      title="Отдельный навык"
+      subtitle="Изучите востребованные технологии, чтобы расширить свой стек и добавить заветную галочку в резюме"
+      [products]="products.byGroup['skill']"
+    />
 
+    <app-product-list
+      title="Интенсивы"
+      subtitle="Экспресс-программы, где за короткий период вы получаете максимум пользы"
+      [products]="products.byGroup['intensive']"
+    />
+
+    <app-product-list
+      title="Бесплатные курсы"
+      subtitle="Необходимые навыки и проекты в портфолио за ваши старания"
+      [products]="products.byGroup['course']"
+    />
+  `,
+})
+
+export class ShopComponent {
+  telegram = inject(TelegramService);
+  products = inject(ProductsService);
+
+  constructor() {
+    this.telegram.MainButton.show();
+    console.log(this.products.byGroup)
+  }
 }
