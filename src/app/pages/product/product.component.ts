f3d28cfbdfbe634c098c5ae14e6488c6825c45f1
MariaMantusova <maria.mantusova@mail.ru>
2024-05-31 13:12:46 +0300

    feat: add product page

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct, ProductsService } from '../../services/products.service';
+import { TelegramService } from '../../services/telegram.service';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  template: `
+   <div class="centered">
+      <h2 class="mb">{{ product.title }}</h2>
+      <br />
+      <img [src]="product.image" [alt]="product.title" />
+      <p>{{ product.text }}</p>
+      <p>{{ product.time }}</p>
+      <a [href]="product.link" target="_blank">Посмотреть курс</a>
+    </div>
+  `,
 })
-export class ProductComponent {
 
+export class ProductComponent {
+  product: IProduct;
+
+  constructor(
+    private products: ProductsService,
+    private telegram: TelegramService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.product = this.products.getById(id);
+  }
 }
